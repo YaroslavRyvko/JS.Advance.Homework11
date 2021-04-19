c1d2ebc376dc9fec7e26ee0411da2be85e198ba8
YaroslavRyvko <slavik.ruvko@gmail.com>
2021-04-19 16:06:08 +0300

    JS.Advance.HW11 Final Commit

---
+++ src/app/hw11/hw11.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Hw11Component } from './hw11.component';
+
+describe('Hw11Component', () => {
+  let component: Hw11Component;
+  let fixture: ComponentFixture<Hw11Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Hw11Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Hw11Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
